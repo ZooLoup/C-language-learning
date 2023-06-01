c087874568d25394898fb196ac441f929fcbcaaa
ZooLoup <116280382+ZooLoup@users.noreply.github.com>
2023-06-01 20:34:25 +0800

    Add files via upload

---
+++ 牛客網作業/牛客網作業/2023-6-1.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//#define MOD(x,y) x%y
+//int main()
+//{
+//	int a = 13;
+//	int b = 94;
+//	printf("%d\n", MOD(b, a + 4));
+//	return 0;
+//	//宏定義只是單純的替換,不涉及運算符
+//}
+
