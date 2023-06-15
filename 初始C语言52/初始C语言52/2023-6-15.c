9a693e0ff07ff00cd62cfe84a5ce41726187bfde
ZooLoup <116280382+ZooLoup@users.noreply.github.com>
2023-06-15 16:02:02 +0800

    Add files via upload

---
+++ 初始C语言52/初始C语言52/2023-6-15.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//struct S
+//{
+//	char name[20];
+//	int age;
+//	double score;
+//};
+//int main()
+//{
+//	//struct S s = { "张三", 20,55.6 };
+//	struct S tmp = { 0 };
+//	FILE* pf = fopen("test.txt", "wb");
+//	if (pf == NULL)
+//	{
+//		return 0;
+//	}
+//	//二进制的形式读文件
+//	fread(&tmp, sizeof(struct S), 1, pf);
+//	printf("%s %d %lf\n", tmp.name, tmp.age, tmp.score);
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+int main()
+{
+	FILE* pf
+}
