2981f3f1df37b4e15b5aa8e45e8ec15ddea81ca8
ZooLoup <116280382+ZooLoup@users.noreply.github.com>
2023-06-16 21:01:32 +0800

    Add files via upload

---
+++ 初始C语言53 (1)/初始C语言53/contact.h
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+//#define MAX 1000
+
+#define MAX_NAME 20
+#define DEFAULT_SZ
+#define MAX_SEX 5
+#define MAX_TELE 12
+#define MAX_ADDR 30
+
+enum Option
+{
+	EXIT,
+	ADD,
+	DEL,
+	SEARCH,
+	MODIFY,
+	SHOW,
+	SORT
+};
+typedef struct PeoInfo
+{
+	char name[MAX_NAME];
+	int age;
+	char sex[MAX_SEX];
+	char tele[MAX_TELE];
+	char addr[MAX_ADDR];
+}PeoInfo;
+
+//通讯录类型
+typedef struct Contact
+{
+	struct PeoInfo *data;//存放1000个信息
+	int size;//记录当前已经有了一个
+	int capacity;//当前通讯录的最大容量
+}Contact;
+
+//声明函数
+//初始化通讯录的函数
+void InitContact(struct Contact* ps);
+//增加一个信息的
+void AddContact(struct Contact* ps);
+//打印通讯录中的信息
+void ShowContact(const struct Contact* ps);
+//删除指定的联系人
+void DelContact(struct Contact* ps);
+//查找指定人的信息
+void SearchContact(const struct Contact* ps);
+
+void ModifyContact(struct Contact* ps);
+
+//排序通讯录内容
+void SrotContact(struct Contact* ps);
+
+void DestroyContact(struct contact* ps);
+//保存文件
+void SaveContact(Contact* ps);
+
+//加载问及那种的信息
+void LoadContact(Contact* ps);
