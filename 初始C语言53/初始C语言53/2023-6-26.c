44b18ff3010624cb84b4effca0e03b5b3954e2d1
ZooLoup <116280382+ZooLoup@users.noreply.github.com>
2023-06-26 17:58:12 +0800

    Add files via upload

---
+++ 初始C语言53/初始C语言53/2023-6-26.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 10 };
+//	FILE* pf = fopen("log.txt", "w");
+//	for(i=0;i)
+//	return 0;
+//}
+
+//#define MAX 100
+//#define STR "hehe"
+//
+//#define ref register
+//#define do_forever for(;;)
+//
+//int main()
+//{
+//	for (;;);
+//}
+
+//#define MAX 100;
+//int main()
+//{
+//	int a = MAX;
+//	printf("%d\n", MAX);
+//	return 0;
+//}
+
+//#define SQUARE(X) X*X
+//int main()
+//{
+//	/*int ret = SQUARE(5);
+//	printf("ret=%d\n", ret);*/
+//	int ret = SQUARE(5 + 1);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+//#define DOUBLE(X) X+X
+//int main()
+//{
+//	int a = 5;
+//	int ret = 10 * DOUBLE(a);
+//	printf("%d\b", ret);
+//	return 0;
+//}
+
+#define CAT(X,Y) X##Y
+
+int main()
+{
+	int Class84 = 2019;
+
+	return 0;
+}
