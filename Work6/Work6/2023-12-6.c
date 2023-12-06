7289f76f0a6f06141c0798a3fba2a42903dd6158
ZooLoup <116280382+ZooLoup@users.noreply.github.com>
2023-12-06 23:16:28 +0800

    Add files via upload

---
+++ Work6/Work6/2023-12-6.c
@@ -0,0 +1,75 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+//int main()
+//{
+//	int arr[3][4];
+//	int i = 0;
+//	int j = 0;
+//	int max = 0;
+//	int min = 0;
+//	max = arr[0][0];
+//	min = arr[0][0];
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (j = 0; j < 4; j++)
+//		{
+//			if (max < arr[i][j])
+//				max = arr[i][j];
+//			if (min > arr[i][j])
+//				min = arr[i][j];
+//		}
+//	}
+//	printf("最大值是%d,最小值是%d",max,min);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr;
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int d = 0;
+//	while ((arr = getchar()) != '\n')
+//	{
+//		if (arr >= 'a' && arr <= 'z' || arr >= 'A' && arr <= 'z')
+//			a++;
+//		else if (arr >= '0' && arr <= '9')
+//			b++;
+//		else if (arr == ' ')
+//			c++;
+//		else
+//			d++;
+//	}
+//	printf("字母为%d个，数字为%d个，空格为%d个，其他为%d个",a,b,c,d);
+//	return 0;
+//}
+
+//#define M 6
+//int main()
+//{
+//	int n = 0;
+//	int i = 0;
+//	int j = 0;
+//	scanf("%d",&n);
+//	int arr[M][M];
+//	int str[M][M];
+//	for (i = 0; i < n; i++)
+//		for (j = 0; j < n; j++)
+//			arr[i][j] = i * n + j + 1;
+//	for (i = 0; i < n; i++)
+//		for (j = 0; j < n; j++)
+//			str[j][i] = arr[i][j];
+//	for (i = 0; i < n; i++)
+//		for (j = 0; j < n; j++)
+//			printf("%d",str[i][j]);
+//	printf("\n");
+//	return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}
