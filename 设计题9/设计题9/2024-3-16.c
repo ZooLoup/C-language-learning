877c9d776fd3822438f4a707371c4a4c6d2ec87e
ZooLoup <116280382+ZooLoup@users.noreply.github.com>
2024-03-17 01:23:16 +0800

    Add files via upload

---
+++ 设计题9/设计题9/2024-3-16.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int n;
+//	scanf("%d",&n);
+//	while (n >= 1 && n <= 13)
+//	{
+//		switch (n)
+//		{
+//		case 1:
+//			printf("Ace");
+//			break;
+//		case 11:
+//			printf("Jack");
+//			break;
+//		case 12:
+//			printf("Queen");
+//			break;
+//		case 13:
+//			printf("King");
+//			break;
+//		default:
+//			printf("%d",n);
+//		}
+//		scanf("%d",&n);
+//	}
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a[10], sum, * p;
+//	for (p = a; p < a + 10; p++)
+//	{
+//		scanf("%d",p);
+//		sum += *p;
+//	}
+//	printf("%.1f",sum/10);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//void trans(char* s)
+//{
+//	char* q = s;
+//	while (*q)
+//		q++;
+//	while (s<q)
+//	{
+//		if (*s >= 'A' && *s <= 'Z')
+//			*s = *s + '32';
+//		s += 2;
+//	}
+//}
+//int main()
+//{
+//	char s[81] = "";
+//	gets(s);
+//	trans(s);
+//	puts(s);
+//	return 0;
+//}
+
